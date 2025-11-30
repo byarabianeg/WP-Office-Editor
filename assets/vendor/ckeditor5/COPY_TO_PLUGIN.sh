2e5d4931fbb0f09c130afa21a13e1bfeed38fad0
byarabianeg <byarabian.eg@gmail.com>
2025-11-30 12:37:42 +0200

    Delete assets directory

--- assets/vendor/ckeditor5/COPY_TO_PLUGIN.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# After building, run this to copy the ckeditor build into the WordPress plugin
-PLUGIN_PATH="../wp-office-editor/assets/vendor/ckeditor5"
-mkdir -p "$PLUGIN_PATH"
-cp build/ckeditor.js "$PLUGIN_PATH/ckeditor.js"
-echo "Copied build/ckeditor.js to $PLUGIN_PATH"
